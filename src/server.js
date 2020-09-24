0cbcd351edd0c6504e271a87118917ad612352a2
yirleison <yppalomeque@gmail.com>
2020-09-24 02:07:24 -0500

    Se actualiza server

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/wakandafoodsweb/index.html'));
 });
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 3000)
