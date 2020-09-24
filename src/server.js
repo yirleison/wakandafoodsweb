22fe2285d1ce7ec7210418cd52bccb75f264d7ac
yirleison <yppalomeque@gmail.com>
2020-09-24 02:21:44 -0500

    Se actualiza server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/wakandafoodsweb'));
-app.get('/',function(req,res){
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname+'/dist/wakandafoodsweb/index.html'));
 });
 
