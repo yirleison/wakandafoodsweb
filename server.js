cdb5c60a088df4d360719b24e185defa35740292
yirleison <yppalomeque@gmail.com>
2020-09-24 13:04:16 -0500

    Se crea proyecto inicial pagina web

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
+const express = require('express');
 const path = require('path');
 const app = express();
-
-app.use(express.static(__dirname+'/dist/wakandafoodsweb'));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/dist/wakandafoodsweb/index.html'));
-});
-
-app.listen(process.env.PORT || 8080)
-
-console.log('Servidor iniciado -------------->', 8080)
+app.use(express.static(__dirname + '/dist/wakandafoodsweb'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/wakandafoodsweb/index.html'));});
+app.listen(process.env.PORT || 8080);
